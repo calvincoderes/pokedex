bf03fafc532a16dbed2b1176895cfb5b0ca566d4
Calvin Coderes <calvin.c.r.coderes@gmail.com>
2023-06-20 08:05:20 +0800

    Hey... This commit is about me trying to figure out tailwind utility classes.  I added the pokeAPI logo along the way.  Cool, cool...

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import mainLogo from "./assets/pokeapi_256.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Catch `em all!</h1>
+    <div className="main-root container sm:mx-auto my-4 flex justify-center">
+      <div
+        id="responsive-main-wrapper"
+        className="w-full sm:w-5/6 lg:w-4/5 xl:w-3/4 h-full "
+      >
+        <div className="flex flex-col gap-4">
+          <div>
+            <img src={mainLogo} className="mx-auto" />
+          </div>
+          <div className="bg-gray-700">content here</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
